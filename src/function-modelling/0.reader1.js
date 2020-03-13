78d6cc10d92c49a69345319c0be1908749bc16b0
zhentian-wan <answer881215@gmail.com>
2020-03-13 23:07:14 +0200

    reader monda self training

---
+++ src/function-modelling/0.reader1.js
@@ -0,0 +1,27 @@
+const Reader = run => ({
+  run,
+  map: f => Reader(x => f(run(x))),
+  chain: f => Reader(x => f(run(x)).run(x)),
+  concat(o) {
+    return Reader(x => run(x).concat(o.run(x)));
+  }
+});
+Reader.of = x => Reader(() => x);
+Reader.ask = Reader(x => x);
+
+const prefix = s => m => `${s}${m}`;
+const prefixHttps = prefix("https://");
+const prefixHttp = prefix("http://");
+
+const res = Reader.of("localhost")
+  .chain(host =>
+    Reader.ask.map(config =>
+      config.https ? prefixHttps(host) : prefixHttp(host)
+    )
+  )
+  .chain(domain =>
+    Reader.ask.map(config => `${domain.concat(":").concat(config.port)}`)
+  )
+  .run({ port: 3000, https: true });
+
+console.log(res); // https://localhost:3000
