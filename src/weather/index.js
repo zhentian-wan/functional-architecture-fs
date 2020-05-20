e102a8962272e2f81a0403ebd67dc90a6acc5169
zhentian-wan <answer881215@gmail.com>
2020-05-20 09:21:48 +0300

    save

---
+++ src/weather/index.js
@@ -0,0 +1,29 @@
+import apiKey from "./apikey";
+import { getWeatherItems } from "./model";
+
+const zip = "55455";
+fetch(
+  `http://api.openweathermap.org/data/2.5/forecast?zip=${zip},us&APPID=${apiKey}`
+);
+/*
+const populateUI = (zip) =>
+  getWeatherItems({ zip, apiKey }).map((weathers) => weathers.map(toLi));
+
+const toLi = (weather) => `<li>${weather.dt} ${weather.temp}</li>`;
+
+///=============================
+const app = () => {
+  const goButton = document.getElementById("go");
+  const input = document.getElementById("zip");
+  const results = document.getElementById("results");
+
+  goButton.addEventListener("click", () => {
+    const zip = input.value.trim();
+    populateUI(zip).fork(console.error, (html) => {
+      results.innerHTML = html;
+    });
+  });
+};
+
+app();
+*/
