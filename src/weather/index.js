c16493f64d2ba0dd58af527dd82b9a48728935db
zhentian-wan <answer881215@gmail.com>
2020-05-21 16:19:48 +0300

    save

--- src/weather/index.js
+++ src/weather/index.js
@@ -1,10 +1,38 @@
 import apiKey from "./apikey";
+import Task from "data.task";
 import { getWeatherItems } from "./model";
-
+import { compose } from "ramda";
 const zip = "55455";
-fetch(
-  `http://api.openweathermap.org/data/2.5/forecast?zip=${zip},us&APPID=${apiKey}`
-);
+const fetchIt = (url) =>
+  new Task((rej, res) => {
+    fetch(
+      `http://api.openweathermap.org/data/2.5/forecast?zip=${zip},us&APPID=${apiKey}`
+    )
+      .then(res)
+      .then((x) => x.json)
+      .catch(rej);
+  });
+
+const makeWeatherUrl = (zip) =>
+  `http://api.openweathermap.org/data/2.5/forecast?zip=${zip},us&APPID=${apiKey}`;
+
+const openWeather = {
+  fetch: compose(fetchIt, makeWeatherUrl),
+};
+
+const app = () => {
+  const goButton = getElementById("go");
+  const input = getElementById("zip");
+  const results = getElementById("results");
+
+  goButton.addEventListener("click", () => {
+    const zipCode = input.value.trim();
+    openWeather.fetch(zipCode).fork(console.error, console.log);
+  });
+};
+
+app();
+
 /*
 const populateUI = (zip) =>
   getWeatherItems({ zip, apiKey }).map((weathers) => weathers.map(toLi));
