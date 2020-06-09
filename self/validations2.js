b30b7b95323054b7d6e47d218bd032b8c9345ad3
zhentian-wan <answer881215@gmail.com>
2020-06-09 09:54:40 +0300

    save

---
+++ self/validations2.js
@@ -0,0 +1,54 @@
+const { List } = require("immutable-ext");
+
+const Success = (x) => ({
+  x,
+  isFail: false,
+  concat(o) {
+    return o.isFail ? o : Success(x);
+  },
+});
+
+const Fail = (x) => ({
+  x,
+  isFail: true,
+  concat(o) {
+    return Fail(o.isFail ? x.concat(o.x) : x);
+  },
+});
+
+const Validation = (run) => ({
+  run,
+  concat(o) {
+    return Validation((key, x) => {
+      return run(key, x).concat(o.run(key, x));
+    });
+  },
+});
+
+// Imp Or And
+
+const isPresent = Validation((key, x) =>
+  !!x ? Success([x]) : Fail([`${key} is required`])
+);
+const isEmail = Validation((key, x) =>
+  /@/.test(x) ? Success([x]) : Fail([`${key} is not an email`])
+);
+
+const obj = { name: "", email: "" };
+const validation = {
+  name: isPresent,
+  email: isPresent.concat(isEmail),
+};
+const validate = (schema, obj) => {
+  return List(Object.keys(schema)).foldMap(
+    (key) => schema[key].run(key, obj[key]),
+    Success([obj])
+  );
+};
+/**
+ * Success: obj
+ *
+ * Error: Array<Error message>
+ */
+const res = validate(validation, obj);
+console.log(res);
